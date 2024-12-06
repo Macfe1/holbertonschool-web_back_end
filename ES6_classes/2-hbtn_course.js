e1fb6c545f81049414b9fc847e14c1242b46ac07
Macfe1 <mac1515.pp@gmail.com>
2024-12-05 19:30:51 -0500

    Adding teh file number 2 adjusting the setters and getters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -15,43 +15,38 @@ export default class HolbertonCourse {
     this._students = students;
   }
 
-  _name = {
-
-    set name(name) {
-      if (typeof name !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-      this._name = name;
-    },
-
-    get name() {
-      return this._name;
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-  };
+    this._name = name;
+  }
 
-  _length = {
-    set length(length) {
-      if (typeof length !== 'number') {
-        throw new TypeError('Length must be a number');
-      }
-      this._length = length;
-    },
+  get name() {
+    return this._name;
+  }
 
-    get length() {
-      return this._length;
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
     }
-  };
+    this._length = length;
+  }
 
-  _students = {
-    set students(students) {
-      if (!Array.isArray(students)) {
-        throw new TypeError('Students must be an array');
-      }
-      this._students = students;
-    },
-  
-    get students() {
-      return this._students;
+  get length() {
+    return this._length;
+  }
+
+
+
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
     }
-  };
+    this._students = students;
+  }
+
+  get students() {
+    return this._students;
+  }
 }
