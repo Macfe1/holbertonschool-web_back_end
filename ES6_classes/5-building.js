8b5b7c7dda921188ddb81dc5534f86cb97ed034a
Macfe1 <mac1515.pp@gmail.com>
2024-12-10 13:29:17 -0500

    Adding the file number 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -9,9 +9,6 @@ export default class Building {
         throw new Error('Class extending Building must override evacuationWarningMessage');
       }    
 
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
     this._sqft = sqft;
   }
 
