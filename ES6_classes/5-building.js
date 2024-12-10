559f53ad56347a11f01317de24388d028d620fa1
Macfe1 <mac1515.pp@gmail.com>
2024-12-10 13:26:09 -0500

    Adding the file number 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,13 +1,13 @@
 export default class Building {
   constructor(sqft) {
 
+    if (this.constructor === Building) {
+      throw new Error('Building is an abstract class and cannot be instantiated directly');
+    }
+
     if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
         throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
-
-    if (this.construtor === Building) {
-      throw new Error('Building is an abstract class and cannot be instantiated directly');
-    } 
+      }    
 
     if (typeof sqft !== 'number') {
       throw new TypeError('sqft must be a number');
