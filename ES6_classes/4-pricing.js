4ccc1a4de1370f0a3188de4e31893a8209ce05d7
Macfe1 <mac1515.pp@gmail.com>
2024-12-09 23:09:46 -0500

    Adding the file number 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-unused-vars */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+
+    if (!(currency instanceof 'Currency')) {
+      throw new TypeError('currency must be a currency');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(currency) {
+    if (typeof currency !== 'number') {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._currency = currency;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
