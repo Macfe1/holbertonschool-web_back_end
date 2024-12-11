7f425d794ee055d466b3857cbc856323ab72db7b
Macfe1 <mac1515.pp@gmail.com>
2024-12-11 18:58:53 -0500

    Adding teh file 1-get_list_student_ids.js

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+const getListStudentIds = (objectArray) => {
+  if (Array.isArray(objectArray)) {
+    return objectArray.map((item) => item.id);
+  }
+  return [];
+};
+export default getListStudentIds;
