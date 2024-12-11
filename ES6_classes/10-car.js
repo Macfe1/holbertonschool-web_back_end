5f76fd8269f382a548236a4c9dd5553e73806a89
Macfe1 <mac1515.pp@gmail.com>
2024-12-10 23:30:47 -0500

    Adding the file 10

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,7 +1,7 @@
 export default class Car {
   constructor(brand, motor, color) {
     if (typeof brand !== 'string') {
-        throw new TypeError('brand must be a string');
+      throw new TypeError('brand must be a string');
     }
     if (typeof motor !== 'string') {
       throw new TypeError('motor must be a string');
