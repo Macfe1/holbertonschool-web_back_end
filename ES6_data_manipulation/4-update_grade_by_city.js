3dd8452052777e76220b78d20fb327399ffca73b
Macfe1 <mac1515.pp@gmail.com>
2024-12-12 18:53:20 -0500

    Adding the file 4-update_grade_by_city.js

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,12 @@
-const updateStudentGradeByCity = (getListStudents, city, newGrades) => {
-  return getListStudents
+const updateStudentGradeByCity = (getListStudents, city, newGrades) => getListStudents
   .filter((item) => item.location === city)
-  .map(item => {
-    const gradeInfo = newGrades.find(grade => grade.studentId === item.id);
+  .map((item) => {
+    const gradeInfo = newGrades.find((grade) => grade.studentId === item.id);
 
     return {
       ...item,
-      grade: gradeInfo ? gradeInfo.grade: 'N/A'
+      grade: gradeInfo ? gradeInfo.grade : 'N/A',
     };
   });
-};
 
 export default updateStudentGradeByCity;
