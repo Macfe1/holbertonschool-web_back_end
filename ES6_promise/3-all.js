fbc68bc5d7909b45713f4774b2e6557abc47ffb3
Macfe1 <mac1515.pp@gmail.com>
2024-12-18 19:18:50 -0500

    Adding the file 3-all.js

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,13 +2,14 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const promise = Promise.all([uploadPhoto(), createUser()]);
-  promise
+  return promise
     .then((response) => {
       const { body, firstName, lastName } = response.reduce((acc, item) => ({
         ...acc, ...item,
       }), {});
-      console.log(body, firstName, lastName);
+      console.log(body, firstName, lastName); 
+    })
+    .catch(() => {
+      console.log("Signup system offline");
     });
 }
-
-handleProfileSignup();
