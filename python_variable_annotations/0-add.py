c70620f5f641628135ef5d42aaf3fabff79544ca
Macfe1 <mac1515.pp@gmail.com>
2025-03-13 17:56:27 -0500

    Adding the README.md and the 0-add.py file

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/binenv pyhton3
+"""
+This module provides a function to add two floating-point numbers.
+"""
+
+
+def add(a: float, b: float) -> float:
+
+    """
+    Adds two float numbers:
+    Args:
+        a (float): Fisrt number
+        b (float): Second number
+    Returns:
+        float: the addition of a and b
+    """
+
+    return a + b
