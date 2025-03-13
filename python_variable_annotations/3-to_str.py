511ed8662044e25000d61e3fc7093b6515803298
Macfe1 <mac1515.pp@gmail.com>
2025-03-13 18:59:35 -0500

    Adding the file 3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+this module provides a function to  turn a float into a string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float into a string.
+
+    Args:
+        n (float): the number
+    Returns:
+        str: The input number converted to a string.
+    """
+    return str(n)
