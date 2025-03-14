3e2b3e884fdbe6da1b6eed7a051b1c5591824a50
Macfe1 <mac1515.pp@gmail.com>
2025-03-13 19:23:26 -0500

    Adding the file 5-sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 This module provides a function that takes a list of floats and
-sums all the numebr in it
+sums all the numbers in it
 """
 
 
@@ -10,8 +10,8 @@ def sum_list(input_list: list[float]) -> float:
     Takes a list of floats and returns their sum.
 
     Args:
-        input_list (list[floats]) : The list of floats
+        input_list (list[float]) : The list of floats
     Returns:
         float: The sum of the list
     """
-    return sum(input_list)
+    return float(sum(input_list))
