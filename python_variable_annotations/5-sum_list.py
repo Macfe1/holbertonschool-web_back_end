e7f689a0a5a5c26ca9c789eeadb59dfb278dd648
Macfe1 <mac1515.pp@gmail.com>
2025-03-13 19:33:38 -0500

    Adding the file 5-sum_list.py I have imported the List from typing

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -4,13 +4,15 @@ This module provides a function that takes a list of floats and
 sums all the numbers in it
 """
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """
     Takes a list of floats and returns their sum.
 
     Args:
-        input_list (list[float]) : The list of floats
+        input_list (List[float]) : The list of floats
     Returns:
         float: The sum of the list
     """
