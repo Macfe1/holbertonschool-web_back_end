56439aafcbbe0a9a354a06e13d7cff0769daa254
Macfe1 <mac1515.pp@gmail.com>
2025-03-19 18:47:58 -0500

    Tweaks in the 0-async_generator file

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,10 +7,10 @@ yields a random float between 0 and 10, ten times, with a
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     An asynchronous generator that yields a random float between
     0 and 10, ten times, with a 1-second delay between each yield.
