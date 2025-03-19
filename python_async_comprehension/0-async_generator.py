eb3f7d7cee88b2a71978f85b6a6ae1216626259e
Macfe1 <mac1515.pp@gmail.com>
2025-03-19 17:41:03 -0500

    tweaks in the 0-async_generator.py file

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -19,6 +19,6 @@ async def async_generator() -> AsyncGenerator[float, None]:
         float: A randomly generated number between 0 and 10.
     """
     for _ in range(10):
-        number = random.uniform(0, 10)
         await asyncio.sleep(1)
+        number = random.uniform(0, 10)
         yield number
