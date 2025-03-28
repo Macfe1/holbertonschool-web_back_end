6bc660d045c248a4dc5275345efed522d8f3b13a
Macfe1 <mac1515.pp@gmail.com>
2025-03-27 19:41:53 -0500

    Adding the file 2-hypermedia_pagination.py with the change in the next page

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -104,7 +104,7 @@ class Server:
         data = self.get_page(page, page_size)
         print("DATA 👁️👁️", data)
         print("LEN DATA 😁", len(data))
-        total_pages = len(data)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
         next_page = None if page + 1 > total_pages else page + 1
         prev_page = page - 1 if page != 1 else None
 
