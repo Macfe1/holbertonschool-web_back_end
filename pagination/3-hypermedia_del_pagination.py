7edef8e0766e3d0d30802954bdce38a70456d714
Macfe1 <mac1515.pp@gmail.com>
2025-03-27 21:40:18 -0500

    Adding the file 3-hypermedia_del_pagination.py adjusting the loop

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -39,8 +39,8 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: Optional[int] = None,
-                        page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+
         """
         Retrieve a page of data from the
         indexed dataset, ensuring resilience
