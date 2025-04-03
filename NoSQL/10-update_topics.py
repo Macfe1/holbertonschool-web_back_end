12c180c470455f2834d54471d51e35235ac9f40b
Macfe1 <mac1515.pp@gmail.com>
2025-04-03 14:27:39 -0500

    Adding the file 10-update_topics.py

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to update the topics
+of a school document in a MongoDB collection.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Updates the topics of a school document based on the school name.
+
+    Args:
+        mongo_collection: The pymongo collection object.
+        name (str): The name of the school to update.
+        topics (list): A list of topics to set for the school.
+
+    Returns:
+        None
+    """
+    mongo_collection.update_one({"name": name},
+                                {"$set": {"topics": topics}})
+
+
+if __name__ == "__main__":
+    pass
