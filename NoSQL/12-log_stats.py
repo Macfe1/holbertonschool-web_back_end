0cdf66d551d9737808ed86a6420248b313b09e9c
Macfe1 <mac1515.pp@gmail.com>
2025-04-03 20:55:33 -0500

    Adding the file 12-log_stats.py using just count_documents

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -13,15 +13,15 @@ from pymongo import MongoClient
 client = MongoClient('mongodb://127.0.0.1:27017')
 collection = client.logs.nginx
 
-total_documentos = collection.find({}).count()
-total_get = collection.find({"method": "GET"}).count()
-total_post = collection.find({"method": "POST"}).count()
-total_put = collection.find({"method": "PUT"}).count()
-total_patch = collection.find({"method": "PATCH"}).count()
-total_delete = collection.find({"method": "DELETE"}).count()
-status = collection.find({"method": "GET", "path": "/status"}).count()
+total_documentos = collection.count_documents({})
+total_get = collection.count_documents({"method": "GET"})
+total_post = collection.count_documents({"method": "POST"})
+total_put = collection.count_documents({"method": "PUT"})
+total_patch = collection.count_documents({"method": "PATCH"})
+total_delete = collection.count_documents({"method": "DELETE"})
+status = collection.count_documents({"method": "GET", "path": "/status"})
 
-print(f"{total_documentos} logs")
+print(total_documentos, "logs")
 print("Methods:")
 print(f"\tmethod GET: {total_get}")
 print(f"\tmethod POST: {total_post}")
@@ -29,6 +29,7 @@ print(f"\tmethod PUT: {total_put}")
 print(f"\tmethod PATCH: {total_patch}")
 print(f"\tmethod DELETE: {total_delete}")
 print(f"{status} status check")
+
 
 if __name__ == "__main__":
     pass
