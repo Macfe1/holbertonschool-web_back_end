fde269bba25e3dca68b37ad502d910bc5a6e0760
Macfe1 <mac1515.pp@gmail.com>
2025-04-03 20:44:17 -0500

    Adding the file 12-log_stats.py

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+This script provides basic statistics about Nginx logs
+stored in a MongoDB collection.
+It connects to the 'logs.nginx' collection and displays:
+- The total number of log documents
+- The number of documents by HTTP method (GET, POST, PUT, PATCH, DELETE)
+- The number of documents with method=GET and path=/status
+"""
+
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+collection = client.logs.nginx
+
+total_documentos = collection.count_documents({})
+total_get = collection.count_documents({"method": "GET"})
+total_post = collection.count_documents({"method": "POST"})
+total_put = collection.count_documents({"method": "PUT"})
+total_patch = collection.count_documents({"method": "PATCH"})
+total_delete = collection.count_documents({"method": "DELETE"})
+status = collection.count_documents({"method": "GET", "path": "/status"})
+
+print(total_documentos, "logs")
+print("Methods:")
+print(f"\tmethod GET: {total_get}")
+print(f"\tmethod POST: {total_post}")
+print(f"\tmethod PUT: {total_put}")
+print(f"\tmethod PATCH: {total_patch}")
+print(f"\tmethod DELETE: {total_delete}")
+print(f"{status} status check")
+
+if __name__ == "__main__":
+    pass
