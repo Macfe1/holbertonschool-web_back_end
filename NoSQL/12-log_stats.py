477eb55c8571f168894cc622b29cbfb71c780891
Macfe1 <mac1515.pp@gmail.com>
2025-04-04 10:26:36 -0500

    12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -16,24 +16,23 @@ the 'nginx' collection. It then calculates and displays:
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    pass
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-collection = client.logs.nginx
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
 
-total_documentos = collection.count_documents({})
-total_get = collection.count_documents({"method": "GET"})
-total_post = collection.count_documents({"method": "POST"})
-total_put = collection.count_documents({"method": "PUT"})
-total_patch = collection.count_documents({"method": "PATCH"})
-total_delete = collection.count_documents({"method": "DELETE"})
-status = collection.count_documents({"method": "GET", "path": "/status"})
+    total_documentos = collection.count_documents({})
+    total_get = collection.count_documents({"method": "GET"})
+    total_post = collection.count_documents({"method": "POST"})
+    total_put = collection.count_documents({"method": "PUT"})
+    total_patch = collection.count_documents({"method": "PATCH"})
+    total_delete = collection.count_documents({"method": "DELETE"})
+    status = collection.count_documents({"method": "GET", "path": "/status"})
 
-print(total_documentos, "logs")
-print("Methods:")
-print(f"\tmethod GET: {total_get}")
-print(f"\tmethod POST: {total_post}")
-print(f"\tmethod PUT: {total_put}")
-print(f"\tmethod PATCH: {total_patch}")
-print(f"\tmethod DELETE: {total_delete}")
-print(f"{status} status check")
+    print(total_documentos, "logs")
+    print("Methods:")
+    print(f"\tmethod GET: {total_get}")
+    print(f"\tmethod POST: {total_post}")
+    print(f"\tmethod PUT: {total_put}")
+    print(f"\tmethod PATCH: {total_patch}")
+    print(f"\tmethod DELETE: {total_delete}")
+    print(f"{status} status check")
