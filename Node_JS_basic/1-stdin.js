5464551c953403b20f3166ab17145e1d30b43d50
Macfe1 <mac1515.pp@gmail.com>
2025-05-26 18:53:58 -0500

    Added 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (input) => {
   const name = input.trim();
-  console.log(`Your name is: ${name}`);
+  console.log(`Your name is: ${name}\n`);
 });
 
 process.stdin.on('end', () => {
