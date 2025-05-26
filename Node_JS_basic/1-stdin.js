e1c9597d0d28ec72efdd2a3f4d725865a7373bec
Macfe1 <mac1515.pp@gmail.com>
2025-05-26 11:38:43 -0500

    Added a new console.log in 1-stin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,5 +3,6 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (input) => {
   const name = input.trim();
   console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
   process.exit();
 });
