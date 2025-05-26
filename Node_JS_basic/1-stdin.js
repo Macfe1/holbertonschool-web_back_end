05de4206a6a484ff2896cd2f8df597e24e695889
Macfe1 <mac1515.pp@gmail.com>
2025-05-26 11:53:26 -0500

    Added 1-stdin.js with and end

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,6 +3,7 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (input) => {
   const name = input.trim();
   console.log(`Your name is: ${name}`);
+  process.stdin.end();
 });
 
 process.stdin.on('end', () => {
