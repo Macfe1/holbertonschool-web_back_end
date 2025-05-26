65f1f853c7a05290b6a2c844abf2f36881fb93da
Macfe1 <mac1515.pp@gmail.com>
2025-05-26 11:47:53 -0500

    Added 1-stdin.js with the end event

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,6 +3,9 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (input) => {
   const name = input.trim();
   console.log(`Your name is: ${name}`);
+});
+
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
   process.exit();
 });
