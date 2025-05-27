e0fe9574671fc10bde049206459dc3c3078676c9
Macfe1 <mac1515.pp@gmail.com>
2025-05-26 19:01:46 -0500

    Added 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (input) => {
-  const name = input.trim();
+  const name = input.toString();
   process.stdout.write(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
